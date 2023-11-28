65874aae1eae4887bfca906ace01cdf03ec88b6d
Chandramani9650 <139802772+Chandramani9650@users.noreply.github.com>
2023-11-28 23:24:11 +0530

    Update bank_Management.js

--- bank_Management.js
+++ bank_Management.js
@@ -1,14 +1,17 @@
-
 function BankAccount(accountNumber, name,type, balance){
     this.accountNumber = accountNumber
     this.name = name
     this.type  = type
     this.balance = balance
-    this.deposit = function(amount){
+    this.active = true
+}
+
+  BankAccount.prototype.deposit = function(amount){
     this.balance+=amount
-    // console.log(this.balance)
-    }
-    this.withdraw = function(amount){
+    console.log("you have deposited: ",this.balance)
+  }
+
+    BankAccount.prototype.withdraw = function(amount){
         if(amount<=this.balance){
             this.balance-=amount
         }else{
@@ -17,24 +20,39 @@ function BankAccount(accountNumber, name,type, balance){
 
         
     }
-    this.checkBalance = function(){
-        console.log(this.balance)
+    BankAccount.prototype.checkBalance = function(){
+        console.log("your balance is:", this.balance)
     }
-    this.isActive = function(){
+    BankAccount.prototype.isActive = function(){
         if(this.balance>0){
-            return true
+            console.log("Account is active")
         }else{
-            return false
+            console.log("Account is not active") 
         }
     }
+    function getTotalBalance(...acc) {
+        return acc.reduce((totalSum, account) => {
+          if (account.active == true) {
+            return (totalSum += account.balance);
+          }
+          return totalSum;
+        }, 0);
+      }
+      
 
 
-}
 
 
 
 let p1 =new BankAccount(1234343,"rajesh", "saving", 400)
+let p2 =new BankAccount(12454343,"rajh", "saving", 800)
 p1.deposit(100)
 p1.withdraw(50)
+p1.deposit(1000)
+p1.withdraw(50)
+
 p1.checkBalance()
-console.log(p1.isActive())
+
+let d = getTotalBalance(p1,p2)
+console.log("Total balance of Backaccount",d)
+
